869fff3779131291074d2c34d594f39d57ef86e8
Frank P <fpinder@comcast.net>
2019-10-03 00:06:26 -0700

    none

--- models/schema.sql
+++ models/schema.sql
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
 DROP DATABASE IF EXISTS foods_db;
 CREATE DATABASE foods_db;
 
-=======
--- DROP DATABASE IF EXISTS exampledb;
--- CREATE DATABASE exampledb;
-
--- DROP DATABASE IF EXISTS testdb;
--- CREATE DATABASE testdb;
-
-
-use foods_db;
-
-drop table foods_db.food;
->>>>>>> 53368a52702946ed47dca05d2bc7b3d2db41ab24
