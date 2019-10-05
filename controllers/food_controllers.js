1a911bc4296dc610b5822ff105dddac7c3bd9e9d
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-10-05 06:14:20 -0700

    declaring variables

---
+++ controllers/food_controllers.js
@@ -0,0 +1,7 @@
+// Import NPM dependency and Express router function.
+var express = require("express");
+var router = express.Router();
+var request = require("request");
+
+// Import data model.
+var db = require("../models");
